1d3f22a1c08fa1e7d7eeb2c454bbdb6c4b50d4da
greninjam26 <94926874+greninjam26@users.noreply.github.com>
2025-07-29 14:57:33 -0400

    create other classes

--- script.js
+++ script.js
@@ -23,6 +23,46 @@ const months = [
 ];
 
 // OOP
+// Untility classes
+class Workout {
+    date = new Date();
+    id = new Date().toISOString.slice(-10);
+    constructor(coords, distance, duration) {
+        this.coords = coords;       // [latitude, longitude]
+        this.distance = distance;   // km
+        this.duration = duration;   // min
+    }
+}
+
+class Running extends Workout {
+    constructor(coords, distance, duration, cadence) {
+        super(coords, distance, duration);
+        this.cadence = cadence;
+        this.calcPace();
+    }
+
+    calcPace() {
+        // min/km
+        this.pace = this.duration / this.distance;
+        return this.pace;
+    }
+}
+
+class Cycling extends Workout {
+    constructor(coords, distance, duration, elevationGain) {
+        super(coords, distance, duration);
+        this.elevationGain = elevationGain;
+        this.calcSpeed();
+    }
+
+    calcSpeed() {
+        // km/h
+        this.speed = this.distance / (this.duration / 60);
+        return this.speed;
+    }
+}
+
+// Main APP
 class APP {
     #map;
     #mapEvent;
